fba6021caf652ee60f0bf02983c8e78ec882680c
javagurulv <javagurulv@gmail.com>
2023-05-15 10:23:02 +0300

    lesson 3

---
+++ lesson_3/dbQueryScript.sql
@@ -0,0 +1,11 @@
+
+
+-- SQL script to insert data to "books" table
+INSERT INTO books(title, author, year_of_issue, page_count)
+VALUES('Три мушкетёра', 'Александр Дюма', 1844, 600);
+
+INSERT INTO books(title, author, year_of_issue, page_count)
+VALUES('Не верьте цифрам!', 'Джон Богл', 2021, 530);
+-- End of SQL script to insert data to "books" table
+
+
