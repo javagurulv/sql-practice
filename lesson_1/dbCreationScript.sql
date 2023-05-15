0d30f3b7fe05c6859936a330faf6d6df843196ab
javagurulv <javagurulv@gmail.com>
2023-05-15 16:59:26 +0300

    lesson 1

--- lesson_1/dbCreationScript.sql
+++ lesson_1/dbCreationScript.sql
@@ -1,6 +1,11 @@
 
+-- SQL script for schema creation
+
 CREATE SCHEMA IF NOT EXISTS sql_practice_lesson_1
 DEFAULT CHARACTER SET utf8 ;
 USE sql_practice_lesson_1;
 
+-- End of SQL script for schema creation
+
 commit;
+
