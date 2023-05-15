239915707671b5a6f50bccb44f7bc18ec5191725
javagurulv <javagurulv@gmail.com>
2023-05-15 12:41:41 +0300

    lesson 3

---
+++ lesson_3/deleteScript.sql
@@ -0,0 +1,11 @@
+
+
+-- SQL script to delete data in table
+
+DELETE FROM books
+WHERE title = "Три мушкетёра";
+
+DELETE FROM readers
+WHERE first_name = "Александр";
+
+-- End of SQL script to delete data in table
