9590117beecbcea5d32d2c6a2a16e1af91a595b4
javagurulv <javagurulv@gmail.com>
2023-05-15 17:01:17 +0300

    lesson 3

--- lesson_3/dbCreationScript.sql
+++ lesson_3/dbCreationScript.sql
@@ -1,12 +1,17 @@
 
+-- SQL script for schema creation
+
 CREATE SCHEMA IF NOT EXISTS sql_practice_lesson_3
 DEFAULT CHARACTER SET utf8 ;
 USE sql_practice_lesson_3;
+
+-- End of SQL script for schema creation
 
 commit;
 
 
 -- SQL script for "readers" table creation
+
 CREATE TABLE readers (
     first_name VARCHAR(100),
     last_name VARCHAR(100),
@@ -15,17 +20,20 @@ CREATE TABLE readers (
     phone VARCHAR(20),
     date_of_birth DATE
 );
+
 -- End of SQL script for "readers" table creation
 
 commit;
 
 -- SQL script for "books" table creation
+
 CREATE TABLE books (
     title VARCHAR(1000),
     author VARCHAR(200),
     year_of_issue INT,
     page_count INT
 );
+
 -- End of SQL script for "books" table creation
 
 commit;
