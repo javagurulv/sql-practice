c7406f618c0ec233e5ab2b4f9631b5f4e3d8c7b0
javagurulv <javagurulv@gmail.com>
2023-05-15 16:55:50 +0300

    lesson 2

--- lesson_2/dbCreationScript.sql
+++ lesson_2/dbCreationScript.sql
@@ -4,3 +4,27 @@ DEFAULT CHARACTER SET utf8 ;
 USE sql_practice_lesson_2;
 
 commit;
+
+-- SQL script for "readers" table creation
+CREATE TABLE readers (
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    personal_code VARCHAR(12),
+    email VARCHAR(100),
+    phone VARCHAR(20),
+    date_of_birth DATE
+);
+-- End of SQL script for "readers" table creation
+
+commit;
+
+-- SQL script for "books" table creation
+CREATE TABLE books (
+    title VARCHAR(1000),
+    author VARCHAR(200),
+    year_of_issue INT,
+    page_count INT
+);
+-- End of SQL script for "books" table creation
+
+commit;
