9268a0005335492c91120e36a2adc8b2c1c70cc5
javagurulv <javagurulv@gmail.com>
2023-05-16 11:38:49 +0300

    lesson 5

--- lesson_5/dbCreationScript.sql
+++ lesson_5/dbCreationScript.sql
@@ -14,12 +14,12 @@ commit;
 
 CREATE TABLE readers (
     id BIGINT AUTO_INCREMENT,
-    first_name VARCHAR(100),
-    last_name VARCHAR(100),
-    personal_code VARCHAR(12),
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    personal_code VARCHAR(12) NOT NULL,
     email VARCHAR(100),
-    phone VARCHAR(20),
-    date_of_birth DATE,
+    phone VARCHAR(20) NOT NULL,
+    date_of_birth DATE NOT NULL,
     PRIMARY KEY(id)
 );
 
@@ -29,9 +29,9 @@ CREATE TABLE readers (
 
 CREATE TABLE books (
     id BIGINT AUTO_INCREMENT,
-    title VARCHAR(1000),
-    author VARCHAR(200),
-    year_of_issue INT,
+    title VARCHAR(200) NOT NULL,
+    author VARCHAR(200) NOT NULL,
+    year_of_issue INT NOT NULL,
     page_count INT,
     PRIMARY KEY(id)
 );
@@ -42,9 +42,9 @@ CREATE TABLE books (
 
 CREATE TABLE library_cards (
     id BIGINT AUTO_INCREMENT,
-    reader_id BIGINT,
-    unique_code VARCHAR(100),
-    date_of_issue DATE,
+    reader_id BIGINT NOT NULL,
+    unique_code VARCHAR(100) NOT NULL,
+    date_of_issue DATE NOT NULL,
     PRIMARY KEY(id),
     FOREIGN KEY(reader_id) REFERENCES readers(id)
 );
@@ -56,9 +56,9 @@ CREATE TABLE library_cards (
 
 CREATE TABLE library_card_items (
     id BIGINT AUTO_INCREMENT,
-    library_card_id BIGINT,
-    book_id BIGINT,
-    issue_date DATE,
+    library_card_id BIGINT NOT NULL,
+    book_id BIGINT NOT NULL,
+    issue_date DATE NOT NULL,
     return_date DATE,
     PRIMARY KEY(id),
     FOREIGN KEY(library_card_id) REFERENCES library_cards(id),
@@ -66,3 +66,37 @@ CREATE TABLE library_card_items (
 );
 
 -- End of SQL script for "library_card_items" table creation
+
+
+-- SQL script for indexes creation
+
+CREATE INDEX index_books_title
+ON books(title);
+
+CREATE INDEX index_books_author
+ON books(author);
+
+CREATE INDEX index_readers_first_name
+ON readers(first_name);
+
+CREATE INDEX index_readers_last_name
+ON readers(last_name);
+
+CREATE INDEX index_readers_personal_code
+ON readers(personal_code);
+
+CREATE INDEX index_readers_phone
+ON readers(phone);
+
+-- End of SQL script for indexes creation
+
+
+-- SQL script to create unique indexes
+
+CREATE UNIQUE INDEX index_books_title_author
+ON books(title, author);
+
+CREATE UNIQUE INDEX index_library_cards_unique_code
+ON library_cards(unique_code);
+
+-- End of SQL script to create unique indexes
