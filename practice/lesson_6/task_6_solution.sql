1d93100517c1ff95a49d7b871d1107b5114be50f
javagurulv <javagurulv@gmail.com>
2023-05-20 15:10:44 +0300

    lesson 6

---
+++ practice/lesson_6/task_6_solution.sql
@@ -0,0 +1,11 @@
+
+-- Variant 1
+SELECT * FROM movies
+WHERE director = "John Lasseter"
+ORDER BY director ASC;
+
+
+-- Variant 2
+SELECT * FROM movies
+WHERE director = "John Lasseter"
+ORDER BY director;
