e969c0e8a8f8bc76b4ba8490a0d968d1f0169e8b
javagurulv <javagurulv@gmail.com>
2023-05-22 14:09:54 +0300

    lesson 10

---
+++ practice/lesson_10/databaseCreationScript.sql
@@ -0,0 +1,41 @@
+
+CREATE SCHEMA IF NOT EXISTS sql_practice_lesson_10
+DEFAULT CHARACTER SET utf8 ;
+USE sql_practice_lesson_10;
+
+commit;
+
+
+CREATE TABLE transactions (
+    id BIGINT AUTO_INCREMENT,
+    broker VARCHAR(100) NOT NULL,
+    city VARCHAR(100) NOT NULL,
+    amount INT NOT NULL,
+    PRIMARY KEY(id)
+);
+
+commit;
+
+
+INSERT INTO transactions(id, broker, city, amount)
+VALUES(1, "John", "Toronto", 100);
+
+INSERT INTO transactions(id, broker, city, amount)
+VALUES(2, "Kiril", "New York", 200);
+
+INSERT INTO transactions(id, broker, city, amount)
+VALUES(3, "Gregor", "Toronto", 50);
+
+INSERT INTO transactions(id, broker, city, amount)
+VALUES(4, "Vasilij", "Los Angeles", 250);
+
+INSERT INTO transactions(id, broker, city, amount)
+VALUES(5, "Karl", "Los Angeles", 199);
+
+INSERT INTO transactions(id, broker, city, amount)
+VALUES(6, "Alexander", "Los Angeles", 49);
+
+INSERT INTO transactions(id, broker, city, amount)
+VALUES(7, "Alexander", "Toronto", 155);
+
+commit;
