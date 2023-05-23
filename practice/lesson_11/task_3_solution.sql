b44efe6fdd518474eb4d124372d036c53bf7153c
javagurulv <javagurulv@gmail.com>
2023-05-23 08:45:26 +0300

    lesson 11

---
+++ practice/lesson_11/task_3_solution.sql
@@ -0,0 +1,9 @@
+
+SELECT movies.*,
+       boxoffices.domestic_sales,
+       boxoffices.international_sales
+FROM movies
+INNER JOIN boxoffices
+ON movies.id = boxoffices.movie_id
+WHERE boxoffices.international_sales > boxoffices.domestic_sales
+ORDER BY movies.year_of_issue ASC;
