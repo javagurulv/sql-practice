059bb012382186b677f1eeedfcf663fe9961839a
javagurulv <javagurulv@gmail.com>
2023-05-24 08:33:43 +0300

    lesson 16

---
+++ practice/lesson_16/task_4_solution.sql
@@ -0,0 +1,27 @@
+
+CREATE TABLE books (
+    id BIGINT AUTO_INCREMENT,
+    title VARCHAR(100) NOT NULL,
+    author VARCHAR(100) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+
+CREATE TABLE readers (
+    id BIGINT AUTO_INCREMENT,
+    full_name VARCHAR(100) NOT NULL,
+    personal_code VARCHAR(100) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+
+CREATE TABLE library_card (
+    id BIGINT AUTO_INCREMENT,
+    reader_id BIGINT NOT NULL,
+    book_id BIGINT NOT NULL,
+    issue_date DATE NOT NULL,
+    return_date DATE NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY (reader_id) REFERENCES readers(id),
+    FOREIGN KEY (book_id) REFERENCES books(id)
+);
