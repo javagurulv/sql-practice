86f34a9cb18abfa6b99ca3ab2b7f0f37d4dbb46f
javagurulv <javagurulv@gmail.com>
2023-05-24 09:17:26 +0300

    lesson 17

---
+++ practice/lesson_17/databaseCreationScript.sql
@@ -0,0 +1,17 @@
+
+CREATE SCHEMA IF NOT EXISTS sql_practice_lesson_17
+DEFAULT CHARACTER SET utf8 ;
+USE sql_practice_lesson_17;
+
+commit;
+
+CREATE TABLE movies (
+    id BIGINT AUTO_INCREMENT,
+    title VARCHAR(100) NOT NULL,
+    director VARCHAR(100) NOT NULL,
+    year_of_issue INT NOT NULL,
+    length_minutes INT NOT NULL,
+    PRIMARY KEY(id)
+);
+
+commit;
