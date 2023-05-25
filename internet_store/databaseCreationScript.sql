fad0fa1cd61861fc4bf252e71a7655314c543abc
javagurulv <javagurulv@gmail.com>
2023-05-25 08:41:12 +0300

    internet_store

--- internet_store/databaseCreationScript.sql
+++ internet_store/databaseCreationScript.sql
@@ -38,6 +38,44 @@ ON client_addresses(client_id);
 commit;
 
 
+CREATE TABLE products (
+    id BIGINT AUTO_INCREMENT,
+    title VARCHAR(100) NOT NULL,
+    manufacturer_country VARCHAR(100) NOT NULL,
+    price DOUBLE NOT NULL,
+    availability INT NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE UNIQUE INDEX index_unique_products_title
+ON products(title);
+
+commit;
+
+
+INSERT INTO products(id, title, manufacturer_country, price, availability)
+VALUES(1, "Milk 1L", "LATVIA", 1.5, 10);
+
+INSERT INTO products(id, title, manufacturer_country, price, availability)
+VALUES(2, "White Bread", "Estonia", 2.0, 100);
+
+INSERT INTO products(id, title, manufacturer_country, price, availability)
+VALUES(3, "iPhone x10", "USA", 1000.0, 99);
+
+INSERT INTO products(id, title, manufacturer_country, price, availability)
+VALUES(4, "iPhone x14", "USA", 1500.0, 77);
+
+INSERT INTO products(id, title, manufacturer_country, price, availability)
+VALUES(5, "Samsung TV", "Korea", 2500.0, 33);
+
+INSERT INTO products(id, title, manufacturer_country, price, availability)
+VALUES(6, "Bose quietcomfort 45", "Korea", 500.0, 44);
+
+INSERT INTO products(id, title, manufacturer_country, price, availability)
+VALUES(7, "Blue pen", "Spain", 10.0, 999);
+
+commit;
+
 INSERT INTO clients(id, full_name, email, phone, registration_date)
 VALUES(1, "Vasilij Ozerov", "ozer@inbox.lv", "+371 2677777", "2010.01.22");
 
