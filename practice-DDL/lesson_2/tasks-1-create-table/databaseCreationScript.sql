70b3cbb19a27d28e349ef97f44765c647b21c756
javagurulv <javagurulv@gmail.com>
2023-06-01 15:24:10 +0300

    ddl

--- practice-DDL/lesson_2/tasks-1-create-table/databaseCreationScript.sql
+++ practice-DDL/lesson_2/tasks-1-create-table/databaseCreationScript.sql
@@ -1,6 +1,7 @@
 
-CREATE SCHEMA IF NOT EXISTS sql_practice_lesson_16
+CREATE SCHEMA IF NOT EXISTS sql_practice_ddl_lesson_2
 DEFAULT CHARACTER SET utf8 ;
-USE sql_practice_lesson_16;
+
+USE sql_practice_ddl_lesson_2;
 
 commit;
